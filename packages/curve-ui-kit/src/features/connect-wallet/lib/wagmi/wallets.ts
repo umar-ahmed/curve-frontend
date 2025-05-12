775f9c8d8dda58d57b492865cb2f15c47e5ed65d
Daniel Schiavini <daniel.schiavini@gmail.com>
2025-05-12 12:34:21 +0200

    chore: remove safe connector from UI
    - users can still connect via the safe UI or WalletConnect

--- packages/curve-ui-kit/src/features/connect-wallet/lib/wagmi/wallets.ts
+++ packages/curve-ui-kit/src/features/connect-wallet/lib/wagmi/wallets.ts
@@ -2,7 +2,6 @@ import createSvgIcon from '@mui/material/utils/createSvgIcon'
 import type { ConnectorType } from '@ui-kit/features/connect-wallet/lib/wagmi/connectors'
 import { BrowserWalletIcon } from '@ui-kit/shared/icons/BrowserWalletIcon'
 import { CoinbaseWalletIcon } from '@ui-kit/shared/icons/CoinbaseWalletIcon'
-import { SafeWalletIcon } from '@ui-kit/shared/icons/SafeWalletIcon'
 import { WalletConnectIcon } from '@ui-kit/shared/icons/WalletConnectIcon'
 
 export type WalletType = {
@@ -15,5 +14,4 @@ export const supportedWallets = [
   { label: `Browser Wallet`, icon: BrowserWalletIcon, connector: 'injected' },
   { label: `Wallet Connect`, icon: WalletConnectIcon, connector: 'walletConnect' },
   { label: `Coinbase`, icon: CoinbaseWalletIcon, connector: 'coinbaseWallet' },
-  { label: 'Safe', icon: SafeWalletIcon, connector: 'safe' },
 ] satisfies WalletType[]
